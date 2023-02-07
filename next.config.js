c4b43a622afbd991aabb9132d69836894c0672f0
Eng-Francisco-Hernandez <fjch87@gmail.com>
2023-02-07 16:59:50 -0600

    chore(workspace): initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
